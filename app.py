24a3aa77059b2f7abf67304c8efe820469c482b2
blitzace90 <ongjunhaojames@gmail.com>
2020-07-08 06:11:58 +0000

    basic crud function

---
+++ app.py
@@ -0,0 +1,145 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import pymongo
+from dotenv import load_dotenv
+from bson import ObjectId
+import os
+import datetime
+
+load_dotenv()
+
+app = Flask(__name__)
+
+MONGO_URI = os.environ.get('MONGO_URI')
+client = pymongo.MongoClient(MONGO_URI)
+
+DB_NAME = "project3_liquor"
+
+SESSION_KEY = os.environ.get('SESSION_KEY')
+
+app.secret_key = SESSION_KEY
+
+# Home Route
+
+
+@app.route('/home')
+def home():
+    return render_template('home.template.html')
+
+# Read route
+
+
+@app.route('/liquor/list')
+def show_liquor():
+    # all_liquor = client[DB_NAME].liquor.find()
+    # liquor_type = client[DB_NAME].liquor.find()
+    search_liquor = request.args.get('search-liquor')
+
+    criteria = {}
+    if search_liquor != "" and search_liquor is not None:
+        criteria['liquor_list'] = {
+            "$regex": search_liquor,
+            "$options": "i"
+        }
+
+    all_liquor = client[DB_NAME].liquor.find(criteria)
+
+    # return render_template('show_liquor.template.html', all_liquor=all_liquor, liquor_type=liquor_type)
+    return render_template('show_liquor.template.html', all_liquor=all_liquor)
+
+# Add route
+
+
+@app.route('/liquor/create')
+def create_liquor():
+    return render_template('create_liquor.template.html')
+
+
+@app.route('/liquor/create', methods=["POST"])
+def process_create_liquor():
+    liquor_name = request.form.get('liquor_name')
+    liquor_type = request.form.get('liquor_type')
+    primary_alcohol = request.form.get('primary_alcohol')
+    serving_method = request.form.get('serving_method')
+    standard_drinkware = request.form.get('standard_drinkware')
+    ingredients = request.form.get('ingredients')
+    preparation = request.form.get('preparation')
+
+    client[DB_NAME].liquor.insert_one({
+        "name": liquor_name,
+        "type": liquor_type,
+        "primary_alcohol": primary_alcohol,
+        "serving_method": serving_method,
+        "standard_drinkware": standard_drinkware,
+        "ingredients": ingredients,
+        "preparation": preparation
+    })
+
+    return redirect(url_for('show_liquor'))
+
+# Update route
+
+
+@app.route('/liquor/update/<id>')
+def update_liquor(id):
+    liquor = client[DB_NAME].liquor.find_one({
+        "_id": ObjectId(id)
+    })
+
+    return render_template("update_liquor.template.html", liquor=liquor)
+
+# Process update route
+
+
+@app.route('/liquor/update/<id>', methods=["POST"])
+def process_update_liquor(id):
+    liquor_name = request.form.get('liquor_name')
+    liquor_type = request.form.get('liquor_type')
+    primary_alcohol = request.form.get('primary_alcohol')
+    serving_method = request.form.get('serving_method')
+    standard_drinkware = request.form.get('standard_drinkware')
+    ingredients = request.form.get('ingredients')
+    preparation = request.form.get('preparation')
+
+    client[DB_NAME].liquor.update_one({
+        "_id": ObjectId(id)
+    }, {
+        "$set": {
+            "name": liquor_name,
+            "type": liquor_type,
+            "primary_alcohol": primary_alcohol,
+            "serving_method": serving_method,
+            "standard_drinkware": standard_drinkware,
+            "ingredients": ingredients,
+            "preparation": preparation
+        }
+    })
+
+    return redirect(url_for('show_liquor'))
+
+# Delete route
+
+
+@app.route('/liquor/delete/<id>')
+def delete_liquor(id):
+    liquor = client[DB_NAME].liquor.find_one({
+        "_id": ObjectId(id)
+    })
+
+    return render_template('confirm_delete_liquor.template.html', liquor=liquor)
+
+# Process delete route
+
+
+@app.route('/liquor/delete/<id>', methods=['POST'])
+def process_delete_liquor(id):
+    client[DB_NAME].liquor.remove({
+        "_id": ObjectId(id)
+    })
+    return redirect(url_for('show_liquor'))
+
+
+# "magic code" -- boilerplate
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
